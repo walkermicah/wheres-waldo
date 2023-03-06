5766bf91f449c8a556c0146131227bdb6a0a1bf1
Walker <walker.m.oliver@gmail.com>
2023-03-06 11:11:23 -0500

    Render GameIntro when app starts

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,11 +1,26 @@
-import './App.scss';
+import { useState } from 'react';
+import GameIntro from '../GameIntro';
+import { Target } from '../../types/types';
+import styles from './App.module.scss';
+import waldo from '../../assets/waldo.jpg';
+import wizard from '../../assets/wizard.jpg';
+import odlaw from '../../assets/odlaw.jpg';
 
-function App() {
+const App = (): JSX.Element => {
+  const targetList: Target[] = [
+    { name: 'Waldo', imgUrl: waldo },
+    { name: 'Wizard', imgUrl: wizard },
+    { name: 'Odlaw', imgUrl: odlaw },
+  ];
+
+  const [gameStatus, setGameStatus] = useState<string>('start');
+  // found
+
   return (
-    <div className="App">
-      <div className="App-container"></div>
+    <div className={styles.App}>
+      {gameStatus === 'start' && <GameIntro targetList={targetList} />}
     </div>
   );
-}
+};
 
 export default App;
