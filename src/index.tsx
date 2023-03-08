c8367c77e8c9deb5ab53b9619e35e5fc0db1b415
Walker <walker.m.oliver@gmail.com>
2023-03-08 09:03:13 -0500

    Change file name

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/App';
-import './styles/global-styles.scss';
+import './styles/index.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as Element);
 root.render(
