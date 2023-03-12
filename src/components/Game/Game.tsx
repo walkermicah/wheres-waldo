31dd2b664ee9265af647e02e16569c2dda42c8d7
Walker <walker.m.oliver@gmail.com>
2023-03-12 14:00:09 -0400

    Center targeting box at click location

--- src/components/Game/Game.tsx
+++ src/components/Game/Game.tsx
@@ -1,36 +1,69 @@
-import { useContext, useState } from 'react';
+import { useContext, useState, useRef } from 'react';
 import AppContext from '../../context/AppContext';
 import TargetingBox from '../TargetingBox';
+import { ClickCoords } from '../../types/types';
 import styles from './Game.module.scss';
 import scene from '../../assets/scene.jpg';
 
 const Game = (): JSX.Element => {
   const [targetStatus, setTargetStatus] = useState<string>('searching');
-  // click location in state
+  const [clickCoords, setClickCoords] = useState<ClickCoords>({
+    x: 0,
+    y: 0,
+  });
+
+  const imageRef = useRef(null);
 
   const toggleTargetStatus = (): void => {
     const newStatus = targetStatus === 'searching' ? 'targeted' : 'searching';
     setTargetStatus(newStatus);
   };
 
-  // function to save click location
+  const getClickCoordinates = (
+    e: React.MouseEvent<HTMLElement>,
+    img: HTMLElement
+  ): void => {
+    // top and left starting points (in px) of image relative to top-left corner of the document
+    const rect = img.getBoundingClientRect();
+    const startImgLeft = rect.left + window.scrollX;
+    const startImgTop = rect.top + window.scrollY;
+
+    // x and y coordinates of click (in px) from the top-left corner of the image
+    const x = e.pageX - startImgLeft;
+    const y = e.pageY - startImgTop;
+
+    // width and height in px of image on screen
+    const width = img.clientWidth;
+    const height = img.clientHeight;
+
+    // percentages
+    const xPercent = (x * 100) / width;
+    const yPercent = (y * 100) / height;
+
+    // update state
+    const coords = { x: xPercent, y: yPercent };
+    setClickCoords(coords);
+  };
 
   const handleClick = (e: React.MouseEvent<HTMLElement>): void => {
-    if ((e.target as HTMLInputElement).id === 'targets') return;
-    // if targetStatus = 'looking' call function to save click location in state
+    const img = imageRef.current;
+    if ((e.target as HTMLElement) !== img) return;
+    targetStatus === 'searching' && getClickCoordinates(e, img);
     toggleTargetStatus();
   };
 
   const { gameStatus } = useContext(AppContext);
+
   const game: string = styles.game;
   const gameOver: string = gameStatus === 'over' ? styles['game-over'] : '';
   const gameImg: string = styles['game-img'];
 
   return (
     <div className={`${game} ${gameOver}`} onClick={handleClick}>
-      <img src={scene} alt="scene" className={gameImg} />
-      {/* render target box at click location (w inline styles?) */}
-      {targetStatus === 'targeted' && <TargetingBox />}
+      <img src={scene} alt="scene" className={gameImg} ref={imageRef} />
+      {targetStatus === 'targeted' && (
+        <TargetingBox clickCoords={clickCoords} />
+      )}
     </div>
   );
 };
--- src/components/TargetingBox/TargetingBox.tsx
+++ src/components/TargetingBox/TargetingBox.tsx
@@ -1,19 +1,33 @@
 import { useContext } from 'react';
+import { ClickCoords } from '../../types/types';
 import AppContext from '../../context/AppContext';
 import styles from './TargetingBox.module.scss';
 
-const TargetingBox = (): JSX.Element => {
+type TargetingBoxProps = {
+  clickCoords: ClickCoords;
+};
+
+const TargetingBox = ({ clickCoords }: TargetingBoxProps): JSX.Element => {
   const { targetList } = useContext(AppContext);
 
   const box: string = styles.box;
   const targets: string = styles.targets;
+  const targetsYOrientation: string =
+    clickCoords.y > 25 ? styles['targets-up'] : styles['targets-down'];
+  const targetsXOrientation: string =
+    clickCoords.x < 50 ? styles['targets-right'] : styles['targets-left'];
   const target: string = styles.target;
   const targetImg: string = styles['target-img'];
 
   return (
     <>
-      <div className={box}>
-        <div className={targets} id="targets">
+      <div
+        className={box}
+        style={{ top: `${clickCoords.y}%`, left: `${clickCoords.x}%` }}
+      >
+        <div
+          className={`${targets} ${targetsYOrientation} ${targetsXOrientation}`}
+        >
           {targetList.map((t) => (
             <div className={target} key={t.name}>
               <img src={t.imgUrl} alt={t.name} className={targetImg} />
