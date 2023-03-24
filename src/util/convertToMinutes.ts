a612a75541170c4b799979c80d4b5312aef49bf1
Walker <walker.m.oliver@gmail.com>
2023-03-24 14:47:28 -0400

    Initial commit

---
+++ src/util/convertToMinutes.ts
@@ -0,0 +1,7 @@
+const convertToMinutes = (seconds: number): string => {
+  const min = Math.floor(seconds / 60).toString();
+  const sec = (seconds % 60).toString().padStart(2, '0');
+  return `${min}:${sec}`;
+};
+
+export default convertToMinutes;
