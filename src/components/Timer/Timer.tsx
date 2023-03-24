f5ed89d38717def6a5edaec38521d2322ce41e3e
Walker <walker.m.oliver@gmail.com>
2023-03-24 17:32:24 -0400

    Show formatted time

--- src/components/Timer/Timer.tsx
+++ src/components/Timer/Timer.tsx
@@ -1,12 +1,13 @@
 import { useContext } from 'react';
 import TimerContext from '../../context/TimerContext';
+import convertToMinutes from '../../util/convertToMinutes';
 import styles from './Timer.module.scss';
 
 const Timer = (): JSX.Element => {
   const { time } = useContext(TimerContext);
   const timer: string = styles.timer;
 
-  return <div className={timer}>{time}</div>;
+  return <div className={timer}>{convertToMinutes(time)}</div>;
 };
 
 export default Timer;
