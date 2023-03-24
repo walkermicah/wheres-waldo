b93c8861ff04570e56a7eed04352698a1ffc4321
Walker <walker.m.oliver@gmail.com>
2023-03-24 17:31:03 -0400

    Refactor

--- src/util/convertToMinutes.ts
+++ src/util/convertToMinutes.ts
@@ -1,5 +1,5 @@
 const convertToMinutes = (seconds: number): string => {
-  const min = Math.floor(seconds / 60).toString();
+  const min = Math.floor(seconds / 60);
   const sec = (seconds % 60).toString().padStart(2, '0');
   return `${min}:${sec}`;
 };
