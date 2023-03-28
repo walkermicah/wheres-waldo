c1c170c5b5025e32683cfa1a1e96c1bd16b12b53
Walker <walker.m.oliver@gmail.com>
2023-03-28 13:46:12 -0400

    Add timer context provider

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/App';
+import { TimerContextProvider } from './context/TimerContext';
 import './styles/index.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as Element);
 root.render(
   <React.StrictMode>
-    <App />
+    <TimerContextProvider>
+      <App />
+    </TimerContextProvider>
   </React.StrictMode>
 );
