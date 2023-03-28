bae7313948097ea459216397f5ae0f9be37c1963
Walker <walker.m.oliver@gmail.com>
2023-03-28 13:47:16 -0400

    Add error handling

--- src/firebase/getTargetCoordinates.ts
+++ src/firebase/getTargetCoordinates.ts
@@ -2,9 +2,13 @@ import { doc, getDoc } from 'firebase/firestore';
 import { db } from './init';
 
 const getTargetCoordinates = async (target: string) => {
-  const docRef = doc(db, 'Target Coordinates', target);
-  const docSnap = await getDoc(docRef);
-  return docSnap.data();
+  try {
+    const docRef = doc(db, 'Target Coordinates', target);
+    const docSnap = await getDoc(docRef);
+    return docSnap.data();
+  } catch (err) {
+    alert(err);
+  }
 };
 
 export default getTargetCoordinates;
--- src/firebase/scores.ts
+++ src/firebase/scores.ts
@@ -3,17 +3,23 @@ import { collection, getDocs, setDoc, doc } from 'firebase/firestore';
 import { db } from './init';
 
 const getScores = async () => {
-  const scores = await getDocs(collection(db, 'Scores'));
-  if (scores) {
+  try {
+    const scores = await getDocs(collection(db, 'Scores'));
     return scores;
+  } catch (err) {
+    alert(err);
   }
 };
 
 const addScore = async (playerName: string, time: number) => {
-  await setDoc(doc(db, 'Scores', uuid()), {
-    name: playerName,
-    time: time,
-  });
+  try {
+    await setDoc(doc(db, 'Scores', uuid()), {
+      name: playerName,
+      time: time,
+    });
+  } catch (err) {
+    alert(err);
+  }
 };
 
 export { getScores, addScore };
